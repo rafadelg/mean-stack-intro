64b4b2070c47a4b7ae713683c37891380e3aa681
manana <manana>
2016-11-17 11:28:13 +0100

    beginning Express

---
+++ javascript/ejemplo08-express/ejm01-base-express.js
@@ -0,0 +1,18 @@
+
+/*
+
+2º prueba, usando app Chrome-Postman
+GET localhost:8080
+
+//construcción del resto de verbos
+//analizo la salida
+console.log(response);
+
+Parametrización de mis url's (proceso de enrutanmto)
+a) base path (index), vía:
+-patrones
+-expresiones regulares: RegExp Object (extender/personalizar; cdo sea need)
+-expresiones regulares extendidas
+b) data-area path (inclusión de datos para facilitar ulterior tratamto)
+-path params
+*/
