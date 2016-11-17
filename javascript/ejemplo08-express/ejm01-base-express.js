96d445f496c01952958a21af702b01790c698b9e
manana <manana>
2016-11-17 13:45:44 +0100

    working w/Express

--- javascript/ejemplo08-express/ejm01-base-express.js
+++ javascript/ejemplo08-express/ejm01-base-express.js
@@ -1,6 +1,6 @@
 
-/*
-
+/* 
+ORGANIZACIÓN DE RUTAS, ENRUTAMTO
 2º prueba, usando app Chrome-Postman
 GET localhost:8080
 
@@ -15,4 +15,25 @@ a) base path (index), vía:
 -expresiones regulares extendidas
 b) data-area path (inclusión de datos para facilitar ulterior tratamto)
 -path params
+
+Tareas (funcionalidades) adicionales/intermedias, concatenadas vía manejadores "handlers"
+Se trata de un proceso previo a la respuesta (antes de servir la petición), ejm:
+- Auditoría (logs)
+- Atenticación (login)
+- acceso a datos (bbdd): procesamto previo
+a) por url de petición
+   next(); para concatenación de manejadores/funciones, se coloca al final de la funcion
+b) por ruta: route()
+c) Definir un objeto router (no se indica ruta alguna): express.Router();
+   +hay q definir cómo el enrutador va a trabajar/comportarse (para una funcionalida; la q sea)
+   +permite generar árboles de enrutamto (cada uno con sus "handlers"; ya no tengo una concatenación única) 
+   +se suelen separar en ficheros (ctes, fras, pedidos...)
+   mapeo/asignación dinámica de paths
+
+   Tratamto de excepciones:
+   ¿compete al router?
+   si: lo trato (error controlado)
+   no: no hago nada. 
+    si es del cte; q se propague al cte  (indicándole, únicamte, q algo ha ido mal
+    si es de la bd; q se propague a la bd
 */
